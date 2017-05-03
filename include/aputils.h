61d5160d17e13e3a6a9716dfb8bf507f73275eb7
Luke Habermehl <luke.habermehl@gmail.com>
2017-05-03 17:49:41 -0400

    Add aputils.h to include common headers

---
+++ include/aputils.h
@@ -0,0 +1,20 @@
+//
+//  aputils.h
+//  libaputils
+//
+//  Created by Luke on 5/3/17.
+//  Copyright © 2017 Luke Habermehl. All rights reserved.
+//
+
+#ifndef aputils_h
+#define aputils_h
+
+#include <aputils/autil_audioprocessingunit.hpp>
+#include <aputils/autil_file.hpp>
+#include <aputils/autil_manager.hpp>
+#include <aputils/autil_number.hpp>
+#include <aputils/autil_parameter.hpp>
+#include <aputils/autil_parametermap.hpp>
+#include <aputils/autil_stringlist.hpp>
+
+#endif /* aputils_h */
